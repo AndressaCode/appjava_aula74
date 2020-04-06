320d93bbd5a73d385bab5a1dba7347e9ae615f40
Andressa <andressaszx@gmail.com>
2020-04-06 00:03:14 -0300

    Finalizando programa

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,57 @@
+/*
+ * Enter product data:
+ * Name: TV
+ * Price: 900.00
+ * Quantity in stock: 10
+ * Product data: TV, $ 900.00, 10 units, Total: $ 9000.00
+ * Enter the number of products to be added in stock: 5
+ * Updated data: TV, $ 900.00, 15 units, Total: $ 13500.00
+ * Enter the number of products to be removed from stock: 3
+ * Updated data: TV, $ 900.00, 12 units, Total: $ 10800.00
+ */
+package application;
+
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andressa Silva
+ */
+public class Program {
+    
+    public static void main(String[] args){
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Product product = new Product();
+                
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+        
+        System.out.print("Product data: " + product);
+        
+        System.out.printf("%n%n");
+        
+        System.out.print("Number of products to be added in the stock: ");
+        int quantity = sc.nextInt();
+        product.addProduct(quantity);
+        
+        System.out.print("Updated data: " + product);
+        
+        System.out.printf("%n%n");
+        System.out.print("Number of products to be removed from the stock: ");
+        quantity = sc.nextInt();
+        product.removeProduct(quantity);
+        System.out.print("Updated data: " + product);
+        
+        sc.close();
+    
+    }    
+}
---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Andressa Silva
+ */
+public class Product {
+    
+    public String name;
+    public double price;
+    public int quantity;
+    
+    public double totalValue(){
+        return price * quantity;
+    }
+    
+    public void addProduct(int quantity){
+        this.quantity += quantity;
+    }
+    
+    public void removeProduct(int quantity){
+        this.quantity -= quantity;
+    }
+        
+    public String toString(){
+        return name + ", $ " 
+                + String.format("%.2f", price)
+                + ", " 
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValue());
+    }
+}
